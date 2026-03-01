d2c26029486eeb7cf2051c56e3c8068f75f9994b
Aayan Sohail <aayan.softwaredev@gmail.com>
2026-03-01 08:20:14 +0500

    Update index.js

--- Customer-Service-Bot/src/index.js
+++ Customer-Service-Bot/src/index.js
@@ -8,9 +8,10 @@ dotenv.config();
 
 app.use(express.json());
 
-// mailtrap will send POST req when email on the redirect email will be received
+// cloudmailin will send POST req when email on the redirect email will be received
 app.post("/webhook/email", email_webhook);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT}`);
 });
+
